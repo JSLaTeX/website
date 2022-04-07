af40060dfc2786fa0dc73f6dd553ed73aafef8fb
Leon Si <contact@leonzalion.com>
2022-04-07 11:39:26 -0400

    fix: lint

--- src/components/jslatex-demo.vue
+++ src/components/jslatex-demo.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { nextTick, onMounted, watch } from 'vue';
+import { onMounted, watch } from 'vue';
 import type * as monaco from 'monaco-editor';
 import { useWindowSize } from '@vueuse/core';
 import { compileJsLatex } from '../utils/latex.js';
