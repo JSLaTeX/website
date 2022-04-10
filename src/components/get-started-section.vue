e1c54d421835925a0dcfda2956a860f79fb77232
Leon Si <contact@leonzalion.com>
2022-04-10 10:05:37 -0400

    ui: use github styling

--- src/components/get-started-section.vue
+++ src/components/get-started-section.vue
@@ -1,62 +1,69 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import 'github-markdown-css';
+import { outdent } from 'outdent';
+
+const calledFromJsExample = outdent`
+	import { compileJsLatex, compileJsLatexFile } from 'jslatex';
+
+	const result = await compileJsLatex(String.raw\`
+	\\documentclass{article}
+	<?= "Hello from EJS!" ?>
+	\`);
+
+	console.log(result);
+	/*
+	Outputs:
+	\\documentclass{article}
+	Hello from EJS!
+	*/
+
+	// In order for dynamic \`import()\`s to work, you need to pass projectBaseUrl:
+	console.log(
+		await compileJsLatexFile({
+			filePath: 'cow.tex',
+			projectBaseUrl: import.meta.url,
+		})
+	);
+`;
+
+const myFileTex = outdent`
+	\\documentclass{article}
+	\\begin{document}
+		<?= "hello".toUpperCase() ?>
+	\\end{document}
+`;
+</script>
 
 <template>
-	<div class="prose max-w-full m-8">
-		<h1 class="font-bold text-lg text-center">Get Started</h1>
-		<h2>Installation</h2>
-		<pre>npm install --global jslatex</pre>
+	<div>
+		<h1 class="font-bold text-5xl text-center">Get Started</h1>
+		<div class="markdown-body m-8">
+			<h2>Installation</h2>
+			<pre>npm install --global jslatex</pre>
 
-		<h2>Command-line Usage</h2>
-		<div>
-			Create a file called <code>myfile.tex</code> in the current directory:
+			<h2>Command-line Usage</h2>
+			<div>
+				Create a file called <code>myfile.tex</code> in the current directory:
+			</div>
+
+			<pre>{{ myFileTex }}</pre>
+
+			<div>Run the following code:</div>
+			<pre>jslatex myfile.tex -o mycompiledfile.tex</pre>
+
+			<h2>Programmatic Usage</h2>
+			<div>JSLaTeX can also be called from JavaScript:</div>
+			<pre>{{ calledFromJsExample }}</pre>
+			<blockquote>
+				<strong>Note:</strong> The
+				<code>jslatex</code>
+				package is ESM-only, meaning it can't be used from CommonJS. Please see
+				<a
+					href="https://gist.github.com/sindresorhus/a39789f98801d908bbc7ff3ecc99d99c"
+					>this gist</a
+				>
+				for more details.
+			</blockquote>
 		</div>
-
-		<pre>
-			\documentclass{article}
-			\begin{document}
-				&lt;?= "hello".toUpperCase() ?&gt;
-			\end{document}
-		</pre
-		>
-
-		<div>Run the following code:</div>
-		<pre>jslatex myfile.tex -o mycompiledfile.tex</pre>
-
-		<h2>Programmatic Usage</h2>
-		<div>JSLaTeX can also be called from JavaScript:</div>
-		<pre>
-			import { compileJsLatex, compileJsLatexFile } from 'jslatex';
-
-const result = await compileJsLatex(String.raw`
-\documentclass{article}
-&lt;?= "Hello from EJS!" ?&gt;
-`);
-
-console.log(result);
-// Outputs:
-/*
-\documentclass{article}
-Hello from EJS!
-*/
-
-// In order for dynamic `import()`s to work, you need to pass projectBaseUrl:
-console.log(
-  await compileJsLatexFile({
-    filePath: 'cow.tex',
-    projectBaseUrl: import.meta.url,
-  })
-);
-		</pre
-		>
-		<blockquote>
-			<strong>Note:</strong> The
-			<code>jslatex</code>
-			package is ESM-only, meaning it can't be used from CommonJS. Please see
-			<a
-				href="https://gist.github.com/sindresorhus/a39789f98801d908bbc7ff3ecc99d99c"
-				>this gist</a
-			>
-			for more details.
-		</blockquote>
 	</div>
 </template>
