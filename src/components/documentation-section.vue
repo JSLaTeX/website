9ba7988617ce0a20136ec831d0eedfb611b71f6b
Leon Si <contact@leonzalion.com>
2022-04-13 22:59:44 -0400

    fix: documentation

--- src/components/documentation-section.vue
+++ src/components/documentation-section.vue
@@ -7,15 +7,23 @@
 			<h2>Tags</h2>
 			<div class="grid grid-cols-[max-content,1fr] gap-x-2 gap-y-1">
 				<code>&lt;?</code>
-				<span>Opening JavaScript tag; doesn't emit output.</span>
+				<span>Scriptlet Opening tag, doesn't emit output.</span>
 				<code>&lt;?_</code>
-				<span>Opening JavaScript tag; removes all whitespace before it.</span>
+				<span
+					>Whitespace-slurping opening tag, removes all whitespace before
+					it.</span
+				>
 				<code>&lt;?=</code>
-				<span>Outputs value into template.</span>
+				<span>Output opening tag, Outputs value into template.</span>
+				<code>?&gt;</code>
+				<span>Ordinary closing tag</span>
 				<code>_?&gt;</code>
-				<span>Closing tag; all whitespace after it.</span>
+				<span
+					>Whitespace-slurping closing tag, removes all whitespace after
+					it.</span
+				>
 				<code>&lt;?#</code>
-				<span>Comment tag; not executed nor outputted.</span>
+				<span>Comment tag, not executed nor outputted.</span>
 			</div>
 		</div>
 	</div>
