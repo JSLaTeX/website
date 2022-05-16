047a56373eb6ff67cb38bad7c830fb9e4d4a7941
Leon Si <contact@leonzalion.com>
2022-05-16 15:36:17 -0400

    refactor: migrate to lionconfig

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,3 @@
-const path = require('path');
-const { defineConfig } = require('eslint-define-config');
+const createESLintConfig = require('lionconfig/eslint')
 
-module.exports = defineConfig({
-	extends: require.resolve('@leonzalion/configs/eslint.cjs'),
-	parserOptions: {
-		project: path.resolve(__dirname, 'tsconfig.eslint.json'),
-		extraFileExtensions: ['vue'],
-	},
-	rules: {
-		'vue/no-v-html': 'off',
-		// See https://github.com/johnsoncodehk/volar/issues/604
-		'@typescript-eslint/no-unused-vars': 'off',
-	},
-});
+module.exports = createESLintConfig(__dirname);
